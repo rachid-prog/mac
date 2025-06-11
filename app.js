16b0b72d0edbc789fd601d455578890e985c1f1a
rachid <12irachid@gmail.com>
2025-06-11 19:26:50 +0200

    port 8080

--- app.js
+++ app.js
@@ -33,7 +33,7 @@ app.use(limiter);
 connexion()
 
 //Port d'écoute
-const PORT = process.env.PORT || 3000
+const PORT = process.env.PORT || 8080
 app.listen(PORT, ()=>{ console.log(`☻ En écoute sur http://localhost:${PORT} ☻`)})
 
 //Les routes
