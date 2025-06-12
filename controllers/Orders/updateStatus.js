df02c64bdd7a4d59371c552b2ef082f51713f20c
rachid <12irachid@gmail.com>
2025-06-12 10:13:36 +0200

    Ajout modification du status livraison

--- controllers/Orders/updateStatus.js
+++ controllers/Orders/updateStatus.js
@@ -2,13 +2,25 @@ const order = require('../../models/Order');
 const updateStatus = async (req, res) => {
     const { id } = req.params;
     const { status } = req.body;
+    const userRole = req.user?.role;
+    //Status autorisées par rôle
+    const rolePermissions = {
+        acceuil: ['en attente', 'préparé'],
+        superviseur: ['en attente', 'préparé'],
+        admin: ['en attente', 'préparé', 'livré'],
+    }
+
 
     try {
-        console.log("Mise à jour du statut de la commande");
+        
         // Vérifier si la commande existe
         const orderToUpdate = await order.findById(id);
         if (!orderToUpdate) {
             return res.status(404).json({ success: false, message: "Commande non trouvée" });
+        }
+        // Vérifier si l'utilisateur a le droit de modifier le statut
+        if (!rolePermissions[userRole] || !rolePermissions[userRole].includes(status)) {
+            return res.status(403).json({ success: false, message: "Vous n'avez pas le droit de modifier ce statut" });
         }
 
         // Mettre à jour le statut de la commande
